568acbf246e6960ce4a5727675f58e22e3f1ba58
Roberto N. Harris <69978828+anthrum@users.noreply.github.com>
2021-09-24 00:44:22 +0200

    Update NormalMixture_SamplingFitting_test1.py
    First test to sampling and fitting Normal Mixture distributions.

--- NormalMixture_SamplingFitting_test1.py
+++ NormalMixture_SamplingFitting_test1.py
@@ -5,6 +5,7 @@ from scipy import stats
 from sklearn import mixture
 import matplotlib.pyplot as plt
 
+# For details on the function bellow check out https://github.com/anthrum/multivariate-mixture-models/blob/main/normal_variance_mixture.ipynb
 def normal_variance_mixture(miu, sigma, n, a, b):
     Z = stats.multivariate_normal.rvs(cov=np.identity(len(sigma)), size=n)  # nx2 matrix
     W = np.sqrt(stats.uniform.rvs(loc=0.5, scale=3, size=n))
@@ -16,14 +17,17 @@ def normal_variance_mixture(miu, sigma, n, a, b):
     X = np.array(miu) + np.matmul(A, Z.transpose()).transpose()  # X = miu + wAZ0 (normal variance mixture)
     return X
 
+#parameters for original mixture model
 SIGMA = np.array([[5,-2],[-2,8]])
 MIU = [50,3]
 N = 10_000
 A = 0.5
 B = 3
 
+
 x = normal_variance_mixture(MIU, SIGMA, N, A, B)
-y = mixture.GaussianMixture(100).fit(x)
+#Fitting normal mixture model with 100 component distribution to the original with continuous uniform mixture rv.
+y = mixture.GaussianMixture(100).fit(x)   
 print(y.sample(N)[0])
 print(x)
 
@@ -32,8 +36,11 @@ print('y \n \n', stats.mstats.describe(y.sample(N)[0]),'\n')
 x = pd.DataFrame(x)
 y = pd.DataFrame(y.sample(N)[0])
 
-#x.hist(bins=100)
-#y.hist(bins=100)
-#x.plot.scatter([0,],[1,], title = 'Original Sample', xlabel = 'M1', ylabel = 'M2')
-#y.plot.scatter([0,],[1,], title = 'Sample from fitted gaussian mixture', xlabel = 'M1', ylabel = 'M2')
-#plt.show()
+x.hist(bins=100)
+y.hist(bins=100)
+x.plot.scatter([0,],[1,], title = 'Original Sample', xlabel = 'M1', ylabel = 'M2')
+y.plot.scatter([0,],[1,], title = 'Sample from fitted gaussian mixture', xlabel = 'M1', ylabel = 'M2')
+plt.show()
+
+# From the sample descriptions and graphical analysis, the two samples seem to be generated from quite similar distributions suggesting the fitted model might
+# be an appropriate.  
