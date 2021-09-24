c74bf77325d671478f928e7057f82f62e0ae7cf1
rebus <anthrum97@gmail.com>
2021-09-24 03:30:47 +0200

    Testing sklearn.mixture.GaussianMixture

---
+++ NormaMixture_Fitting_test2.py
@@ -0,0 +1,32 @@
+sp500 = pd.read_csv('HistoricalData_1632437651658.csv')
+gold = pd.read_csv('HistoricalData_1632440949562.csv').drop([0])
+
+print(sp500)
+print(gold)
+
+
+gold1 = gold['Date']
+
+# cleaning and fitting dates/index
+comparison = sp500['Data'].reset_index(drop=True) == gold1.drop([len(gold1)],  axis = 0).reset_index(drop=True)
+gold.index = np.arange(0, len(gold))
+for i in range(len(comparison)):
+    if comparison[i] == False:
+        print(i)
+        gold = gold.drop([i], axis = 0)
+        break
+    else:
+        pass
+
+gold.reset_index(drop=True, inplace=True)
+print(gold)
+print(sp500)
+# number index matches
+
+# Calculating returns (we're calculating daily log returns considering the difference between closing and opening prices
+sp500_log_returns = np.log(sp500.iloc[:, 1]) - np.log(sp500.iloc[:, 3])
+gold_log_returns = np.log(gold.iloc[:, 1]) - np.log(gold.iloc[:, 3])
+returns = pd.DataFrame({'sp500 returns':sp500_log_returns,'gold returns': gold_log_returns})
+print(returns)
+#print(y)
+z = mixture.GaussianMixture(100).fit(returns)
--- NormalMixture_SamplingFitting_test1.py
+++ NormalMixture_SamplingFitting_test1.py
@@ -32,41 +32,9 @@ print('y \n \n', stats.mstats.describe(y.sample(N)[0]),'\n')
 x = pd.DataFrame(x)
 y = pd.DataFrame(y.sample(N)[0])
 
-#x.hist(bins=100)
-#y.hist(bins=100)
-#x.plot.scatter([0,],[1,], title = 'Original Sample', xlabel = 'M1', ylabel = 'M2')
-#y.plot.scatter([0,],[1,], title = 'Sample from fitted gaussian mixture', xlabel = 'M1', ylabel = 'M2')
-#plt.show()
+x.hist(bins=100)
+y.hist(bins=100)
+x.plot.scatter([0,],[1,], title = 'Original Sample', xlabel = 'M1', ylabel = 'M2')
+y.plot.scatter([0,],[1,], title = 'Sample from fitted gaussian mixture', xlabel = 'M1', ylabel = 'M2')
+plt.show()
 
-sp500 = pd.read_csv('HistoricalData_1632437651658.csv')
-gold = pd.read_csv('HistoricalData_1632440949562.csv').drop([0])
-
-print(sp500)
-print(gold)
-
-
-gold1 = gold['Date']
-
-# cleaning and fitting dates/index
-comparison = sp500['Data'].reset_index(drop=True) == gold1.drop([len(gold1)],  axis = 0).reset_index(drop=True)
-gold.index = np.arange(0, len(gold))
-for i in range(len(comparison)):
-    if comparison[i] == False:
-        print(i)
-        gold = gold.drop([i], axis = 0)
-        break
-    else:
-        pass
-
-gold.reset_index(drop=True, inplace=True)
-print(gold)
-print(sp500)
-# number index matches
-
-# Calculating returns (we're calculating daily log returns considering the difference between closing and opening prices
-sp500_log_returns = np.log(sp500.iloc[:, 1]) - np.log(sp500.iloc[:, 3])
-gold_log_returns = np.log(gold.iloc[:, 1]) - np.log(gold.iloc[:, 3])
-returns = pd.DataFrame({'sp500 returns':sp500_log_returns,'gold returns': gold_log_returns})
-print(returns)
-#print(y)
-z = mixture.GaussianMixture(100).fit(returns)
